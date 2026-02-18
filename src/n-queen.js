eed330735b27f8f24fdc6c29cc81eacc2aa546fd
HerbertNtim <herbertntim15@gmail.com>
2026-02-18 13:11:07 +0000

    working on mid-sem tt

--- src/n-queen.js
+++ src/n-queen.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const examsPath = path.join(__dirname, "./outputs/rooms.json");
 const examsSchedule = JSON.parse(fs.readFileSync(examsPath, "utf-8"));
-const tasPath = path.join(__dirname, "./outputs/rooms.json");
+const tasPath = path.join(__dirname, "./outputs/tas_list.json");
 const TAs = JSON.parse(fs.readFileSync(tasPath, "utf-8"));
 
 class ScheduleTAs {
@@ -36,7 +36,17 @@ class ScheduleTAs {
     console.log(`Total Sessions for TAs: ${totalSession}`);
     return totalSession;
   }
+
+  getAverageSession(examsSchedule, tasList) {
+    const totalSession = this.getTotalSessions(examsSchedule);
+    const totalTAs = tasList.length;
+
+    console.log(`Total TAs: ${totalTAs}`);
+    console.log(`Average Schedule: ${totalSession / totalTAs}`);
+
+    return Math.floor(totalSession / totalTAs);
+  }
 }
 
 const exams = new ScheduleTAs();
-console.log(exams.getTotalSessions(examsSchedule));
+console.log(exams.getAverageSession(examsSchedule, TAs));
